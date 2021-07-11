6f727f349887083f4d9dc585676c26ea01d908b0
rezaejaeja <reza.aprian@traveloka.com>
2021-07-11 20:34:44 +0700

    Update Gradle, Add Test Activity

--- app/src/main/java/com/aprionstudio/submission3/Main/NewTestActivity.kt
+++ app/src/main/java/com/aprionstudio/submission3/Main/NewTestActivity.kt
@@ -1,6 +1,12 @@
 package com.aprionstudio.submission3.Main
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 class NewTestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var test = "test"
+    }
 }
